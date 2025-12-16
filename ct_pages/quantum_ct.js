8f4e58771491ed732347e818ca65c12efa3f820f
asendia2025 <aseng@ase-semi.com>
2025-12-16 21:49:02 +0900

    Add files via upload

---
+++ ct_pages/quantum_ct.js
@@ -0,0 +1,506 @@
+const QUANTUM_CT = [
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 1,
+    "desc": "Parts Preparation",
+    "ct": 70
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 2,
+    "desc": "Front Panel",
+    "ct": 7
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 3,
+    "desc": "(PQC) Front Panel",
+    "ct": 3
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 4,
+    "desc": "Rear Panel",
+    "ct": 11
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 5,
+    "desc": "(PQC) Rear Panel",
+    "ct": 4
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 6,
+    "desc": "VI Sensor",
+    "ct": 45
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 7,
+    "desc": "(PQC) VI Sensor",
+    "ct": 5
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 8,
+    "desc": "Heatsink Mounting Plate",
+    "ct": 20
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 9,
+    "desc": "(PQC) Heatsink Mounting Plate",
+    "ct": 6
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 10,
+    "desc": "VI Sensor + Heatsink Mounting Plate",
+    "ct": 12
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 11,
+    "desc": "(PQC) VI Sensor + Heatsink Mounting Plate",
+    "ct": 3
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 12,
+    "desc": "HF RF Tray",
+    "ct": 45
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 13,
+    "desc": "(PQC) HF RF Tray",
+    "ct": 6
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 14,
+    "desc": "Heat Sink",
+    "ct": 10
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 15,
+    "desc": "(PQC) Heat Sink",
+    "ct": 4
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 16,
+    "desc": "VISMP +Heat Sink + HF RF Tray",
+    "ct": 12
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 17,
+    "desc": "(PQC) VISMP +Heat Sink + HF RF Tray",
+    "ct": 3
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 18,
+    "desc": "Control Panel",
+    "ct": 44
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 19,
+    "desc": "(PQC) Control Panel",
+    "ct": 7
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 20,
+    "desc": "SHIELD PANEL",
+    "ct": 12
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 21,
+    "desc": "(PQC) SHIELD PANEL",
+    "ct": 3
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 22,
+    "desc": "HK POWER SUPPLY",
+    "ct": 7
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 23,
+    "desc": "(PQC) HK POWER SUPPLY",
+    "ct": 3
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 24,
+    "desc": "CONTROL BUCKET",
+    "ct": 34
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 25,
+    "desc": "(PQC) CONTROL BUCKET",
+    "ct": 8
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 26,
+    "desc": "Bottom Panel",
+    "ct": 10
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 27,
+    "desc": "(PQC) Bottom Panel",
+    "ct": 2
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 28,
+    "desc": "HV DC POWER SUPPLY",
+    "ct": 6
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 29,
+    "desc": "(PQC) HV DC POWER SUPPLY",
+    "ct": 2
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 30,
+    "desc": "Shunt Array",
+    "ct": 90
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 31,
+    "desc": "(PQC) Shunt Array",
+    "ct": 10
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 32,
+    "desc": "Pre Test ",
+    "ct": 160
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 33,
+    "desc": "(PQC) Pre Test ",
+    "ct": 15
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 34,
+    "desc": "Post Test",
+    "ct": 50
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 35,
+    "desc": "(PQC) Post Test",
+    "ct": 10
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 36,
+    "desc": "Packing",
+    "ct": 20
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 1,
+    "desc": "Preparing Templete",
+    "ct": 20
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 2,
+    "desc": "RS 232 Communication",
+    "ct": 2
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 3,
+    "desc": "Upgrade Firmware",
+    "ct": 13
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 4,
+    "desc": "Linear Power Supply Calibration",
+    "ct": 11
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 5,
+    "desc": "Fan Interlock and Temperature Test",
+    "ct": 10
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 6,
+    "desc": "Check LED",
+    "ct": 2
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 7,
+    "desc": "Front Panel Interlock Test",
+    "ct": 1
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 8,
+    "desc": "Bias Check Test",
+    "ct": 6
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 9,
+    "desc": "Attenuation Test",
+    "ct": 11
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 10,
+    "desc": "Contour Zm Adjustment",
+    "ct": 22
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 11,
+    "desc": "High Frequency S-Map Collection",
+    "ct": 68
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 12,
+    "desc": "High Frequency S-Map Validation",
+    "ct": 3
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 13,
+    "desc": "Low Frequency S-Map Collection",
+    "ct": 68
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 14,
+    "desc": "Low Frequency S-Map Validation",
+    "ct": 3
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 15,
+    "desc": "Generate Caps and Zpar",
+    "ct": 48
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 16,
+    "desc": "Low Frequency Phase Map",
+    "ct": 15
+  },
+  {
+    "type": "TEST",
+    "sub": "FUNCTION",
+    "no": 17,
+    "desc": "Low Frequency Phase Calibration",
+    "ct": 20
+  },
+  {
+    "type": "TEST",
+    "sub": "FUNCTION",
+    "no": 18,
+    "desc": "DC Bias Calibration",
+    "ct": 10
+  },
+  {
+    "type": "TEST",
+    "sub": "FUNCTION",
+    "no": 19,
+    "desc": "High Frequency Phase Map",
+    "ct": 15
+  },
+  {
+    "type": "TEST",
+    "sub": "FUNCTION",
+    "no": 20,
+    "desc": "High Frequency Phase Calibration",
+    "ct": 20
+  },
+  {
+    "type": "TEST",
+    "sub": "FUNCTION",
+    "no": 21,
+    "desc": "Find Best Tune Position",
+    "ct": 30
+  },
+  {
+    "type": "TEST",
+    "sub": "FUNCTION",
+    "no": 22,
+    "desc": "High Frequency Voltage Calibration",
+    "ct": 37
+  },
+  {
+    "type": "TEST",
+    "sub": "FUNCTION",
+    "no": 23,
+    "desc": "Low Frequency Voltage Calibration",
+    "ct": 31
+  },
+  {
+    "type": "TEST",
+    "sub": "FUNCTION",
+    "no": 24,
+    "desc": "DAC Calibration",
+    "ct": 6
+  },
+  {
+    "type": "TEST",
+    "sub": "FUNCTION",
+    "no": 25,
+    "desc": "Load Configuration File",
+    "ct": 17
+  },
+  {
+    "type": "TEST",
+    "sub": "FUNCTION",
+    "no": 26,
+    "desc": "Verify Unit S/N",
+    "ct": 5
+  },
+  {
+    "type": "TEST",
+    "sub": "FUNCTION",
+    "no": 27,
+    "desc": "Verify Control Board S/N",
+    "ct": 5
+  },
+  {
+    "type": "TEST",
+    "sub": "FUNCTION",
+    "no": 28,
+    "desc": "VI Sensor Validation",
+    "ct": 23
+  },
+  {
+    "type": "TEST",
+    "sub": "FUNCTION",
+    "no": 29,
+    "desc": "Auto Match Validation Test",
+    "ct": 17
+  },
+  {
+    "type": "TEST",
+    "sub": "FUNCTION",
+    "no": 30,
+    "desc": "Preset Validation",
+    "ct": 16
+  },
+  {
+    "type": "TEST",
+    "sub": "FUNCTION",
+    "no": 31,
+    "desc": "DeviceNet Validation",
+    "ct": 15
+  },
+  {
+    "type": "TEST",
+    "sub": "FUNCTION",
+    "no": 32,
+    "desc": "Burn-in",
+    "ct": 123
+  },
+  {
+    "type": "TEST",
+    "sub": "FUNCTION",
+    "no": 33,
+    "desc": "Parameter Validation",
+    "ct": 15
+  },
+  {
+    "type": "TEST",
+    "sub": "FUNCTION",
+    "no": 34,
+    "desc": "Create Backup file",
+    "ct": 5
+  },
+  {
+    "type": "TEST",
+    "sub": "FUNCTION",
+    "no": 35,
+    "desc": "Migration Data to SPC tool",
+    "ct": 15
+  },
+  {
+    "type": "TEST",
+    "sub": "TROUBLESHOOTING",
+    "no": 36,
+    "desc": "(Target) FPY 90% / Recovery Days 7Hr ",
+    "ct": 42
+  }
+];
---
+++ ct_pages/tykon_ct.js
@@ -0,0 +1,513 @@
+const TYKON_CT = [
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 1,
+    "desc": "Parts Preparation",
+    "ct": 70
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 2,
+    "desc": "Front Panel",
+    "ct": 10
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 3,
+    "desc": "(PQC) Front Panel",
+    "ct": 5
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 4,
+    "desc": "Rear Panel",
+    "ct": 6
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 5,
+    "desc": "(PQC) Rear Panel",
+    "ct": 2
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 6,
+    "desc": "VI Sensor",
+    "ct": 30
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 7,
+    "desc": "(PQC) VI Sensor",
+    "ct": 5
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 8,
+    "desc": "Heatsink PA",
+    "ct": 35
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 9,
+    "desc": "(PQC) Heatsink PA",
+    "ct": 8
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 10,
+    "desc": "Heat Sink PA + VI Sensor",
+    "ct": 7
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 11,
+    "desc": "(PQC) Heat Sink PA + VI Sensor",
+    "ct": 2
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 12,
+    "desc": "RF Tray ",
+    "ct": 30
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 13,
+    "desc": "(PQC) RF Tray",
+    "ct": 5
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 14,
+    "desc": "HSK Power Supply",
+    "ct": 9
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 15,
+    "desc": "(PQC) HSK Power Supply",
+    "ct": 2
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 16,
+    "desc": "Left Shield",
+    "ct": 15
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 17,
+    "desc": "(PQC) Left Shield",
+    "ct": 3
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 18,
+    "desc": "Terminal Block",
+    "ct": 7
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 19,
+    "desc": "(PQC) Terminal Block",
+    "ct": 2
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 20,
+    "desc": "Control Bucket",
+    "ct": 30
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 21,
+    "desc": "(PQC) Control Bucket",
+    "ct": 3
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 22,
+    "desc": "Bottom Panel",
+    "ct": 9
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 23,
+    "desc": "(PQC) Bottom Panel",
+    "ct": 2
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 24,
+    "desc": "BP Power Supply",
+    "ct": 4
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 25,
+    "desc": "(PQC) BP Power Supply",
+    "ct": 2
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 26,
+    "desc": "HVDC Power Supply",
+    "ct": 5
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 27,
+    "desc": "(PQC) HVDC Power Supply",
+    "ct": 2
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 28,
+    "desc": "Surpport Panel",
+    "ct": 9
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 29,
+    "desc": "(PQC) Surpport Panel",
+    "ct": 3
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 30,
+    "desc": "RF To Heatsink",
+    "ct": 20
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 31,
+    "desc": "(PQC) RF To Heatsink",
+    "ct": 5
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 32,
+    "desc": "Shunt Array",
+    "ct": 50
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 33,
+    "desc": "(PQC) Shunt Array",
+    "ct": 5
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 34,
+    "desc": "Pre Test ",
+    "ct": 135
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 35,
+    "desc": "(PQC) Pre Test ",
+    "ct": 10
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 36,
+    "desc": "Post Test",
+    "ct": 50
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "PQC",
+    "no": 37,
+    "desc": "(PQC) Post Test",
+    "ct": 10
+  },
+  {
+    "type": "ASSEMBLY",
+    "sub": "",
+    "no": 38,
+    "desc": "Packing",
+    "ct": 20
+  },
+  {
+    "type": "TEST",
+    "sub": "PA TEST",
+    "no": 1,
+    "desc": "PA TEST",
+    "ct": 40
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 2,
+    "desc": "Preparing Templete",
+    "ct": 20
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 3,
+    "desc": "Verify Unit S/N",
+    "ct": 5
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 4,
+    "desc": "Verify Control Board S/N",
+    "ct": 5
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 5,
+    "desc": "RS 232 Communication",
+    "ct": 2
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 6,
+    "desc": "Verify DSP version",
+    "ct": 5
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 7,
+    "desc": "Verify FPGA Firmware",
+    "ct": 5
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 8,
+    "desc": "Upload Default Configuration File",
+    "ct": 3
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 9,
+    "desc": "Verify Temp Sensor Reading",
+    "ct": 2
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 10,
+    "desc": "Interlock Alarm",
+    "ct": 2
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 11,
+    "desc": "Interlock Fan * 3",
+    "ct": 10
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 12,
+    "desc": "Housekeeping voltage calibration",
+    "ct": 2
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 13,
+    "desc": "HVDC Calibration ",
+    "ct": 2
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 14,
+    "desc": "Display Board Check",
+    "ct": 2
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 15,
+    "desc": "Bias Current Test",
+    "ct": 6
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 16,
+    "desc": "Cal DC bias voltage",
+    "ct": 10
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 17,
+    "desc": "Perform Contour",
+    "ct": 20
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 18,
+    "desc": "Perform Smap",
+    "ct": 68
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 19,
+    "desc": "Smap Validation",
+    "ct": 3
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 20,
+    "desc": "Convert to Zpar and Caps files",
+    "ct": 36
+  },
+  {
+    "type": "TEST",
+    "sub": "INITIAL",
+    "no": 21,
+    "desc": "Phase map",
+    "ct": 15
+  },
+  {
+    "type": "TEST",
+    "sub": "FUNCTION",
+    "no": 22,
+    "desc": "VI Calibration",
+    "ct": 15
+  },
+  {
+    "type": "TEST",
+    "sub": "FUNCTION",
+    "no": 23,
+    "desc": "VI Validation",
+    "ct": 10
+  },
+  {
+    "type": "TEST",
+    "sub": "FUNCTION",
+    "no": 24,
+    "desc": "Phase Calibration",
+    "ct": 20
+  },
+  {
+    "type": "TEST",
+    "sub": "FUNCTION",
+    "no": 25,
+    "desc": "Phase Validation",
+    "ct": 10
+  },
+  {
+    "type": "TEST",
+    "sub": "FUNCTION",
+    "no": 26,
+    "desc": "DAC Calibration",
+    "ct": 6
+  },
+  {
+    "type": "TEST",
+    "sub": "FUNCTION",
+    "no": 27,
+    "desc": "Probe Calibration",
+    "ct": 15
+  },
+  {
+    "type": "TEST",
+    "sub": "FUNCTION",
+    "no": 28,
+    "desc": "Vpp Vcap calibration",
+    "ct": 20
+  },
+  {
+    "type": "TEST",
+    "sub": "FUNCTION",
+    "no": 29,
+    "desc": "Parameter validation",
+    "ct": 15
+  },
+  {
+    "type": "TEST",
+    "sub": "FUNCTION",
+    "no": 30,
+    "desc": "Auto Match Test",
+    "ct": 10
+  },
+  {
+    "type": "TEST",
+    "sub": "FUNCTION",
+    "no": 31,
+    "desc": "Burn In",
+    "ct": 123
+  },
+  {
+    "type": "TEST",
+    "sub": "FUNCTION",
+    "no": 32,
+    "desc": "Create Backup File",
+    "ct": 10
+  },
+  {
+    "type": "TEST",
+    "sub": "FUNCTION",
+    "no": 33,
+    "desc": "Verify DeviceNet or ECAT(If applicable)",
+    "ct": 15
+  },
+  {
+    "type": "TEST",
+    "sub": "FUNCTION",
+    "no": 34,
+    "desc": "Migration Data to SPC tool",
+    "ct": 15
+  },
+  {
+    "type": "TEST",
+    "sub": "TROUBLESHOOTING",
+    "no": 35,
+    "desc": "(Target) FPY 90% / Recovery Days 7Hr ",
+    "ct": 42
+  }
+];
